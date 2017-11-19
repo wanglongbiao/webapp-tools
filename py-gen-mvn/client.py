71ebb7d440e37d51d0cc871791089e79be9242db
wang long biao <wanglongbiao@vip.qq.com>
2017-11-20 07:53:26 +0800

    add log4j

--- py-gen-mvn/client.py
+++ py-gen-mvn/client.py
@@ -11,10 +11,10 @@ import re
 # from Tkinter import Tk
 # import Pyperclip
 
+# 程序的开始，面向过程设计
 def start():
     default_path = 'D:\\eclipse_workspace\\'
     print("\n######## default path:%s ########\n" % default_path)
-
     default_group_id = 'me.wang'
     default_artifact_id = 'test-webapp1'
 
@@ -33,37 +33,47 @@ def start():
               '-DgroupId=' + group_id + ' -DartifactId=' + artifact_id
 
     os.system(command)
-    os.makedirs('./' + artifact_id + '/src/main/java/%s' % group_id.replace('.', '/'))
+
+    create_directories(artifact_id, group_id)
+
+    copy_files(artifact_id, group_id)
+
+    move_project(artifact_id, default_path)
+
+
+# 创建默认的文件夹
+def create_directories(artifact_id, group_id):
+    default_package = './' + artifact_id + '/src/main/java/%s' % group_id.replace('.', '/')
+    print("default_package:%s" % default_package)
+
+    os.makedirs(default_package + '/entity')
+    os.makedirs(default_package + '/service')
+    os.makedirs(default_package + '/repository')
+    os.makedirs(default_package + '/controller')
     os.makedirs('./' + artifact_id + '/src/test/java')
 
+
+# 复制资源文件到项目中
+def copy_files(artifact_id, group_id):
     et.register_namespace('', 'http://maven.apache.org/POM/4.0.0')
     pom_doc = et.parse('./resources/pom.xml')
     modify_pom(pom_doc, 'artifactId', artifact_id)
     modify_pom(pom_doc, 'groupId', group_id)
     modify_pom(pom_doc, 'name', artifact_id + ' Maven Webapp')
     modify_pom(pom_doc, 'finalName', artifact_id)
-
     pom_doc.write('./%s/pom.xml' % artifact_id)
-
     shutil.copy('./resources/web.xml', './' + artifact_id + '/src/main/webapp/WEB-INF')
-    # shutil.copy('./resources/pom.xml', './' + artifact_id)
 
-    # os.system("explorer.exe ./")
-    # current_path = os.getcwd()
-    # print(current_path)
-    # os.system("explorer.exe %s" % current_path)
 
+def move_project(artifact_id, default_path):
     shutil.move('./' + artifact_id, default_path)
     new_path = default_path + artifact_id
     print("new path:" + new_path)
     os.system("explorer.exe %s" % new_path)
-    # pyperclip.copy(new_path)
-
-    # open_directory()
 
 
 def get_namespace(element):
-    m = re.match('\{.*\}', element.tag)
+    m = re.match('{.*\}', element.tag)
     return m.group(0) if m else ''
 
 
