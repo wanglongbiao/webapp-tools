b2080fee8a46697064dfcb6e4e179257f8e72421
WangLongBiao <wanglongbiao2000@163.com>
2018-04-23 16:31:32 +0800

    add InitMyUbuntu2018

---
+++ InitMyUbuntu2018/install-paper-theme.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:snwh/pulp
+sudo apt-get update
+sudo apt-get install paper-gtk-theme
+sudo apt-get install paper-icon-theme
+sudo apt-get install paper-cursor-theme
---
+++ InitMyUbuntu2018/my-init.sh
@@ -0,0 +1,63 @@
+
+
+echo "update..."
+#sudo apt-get update
+
+echo "install vim..."
+sudo apt-get install -y vim
+
+echo "alias vi=vim"
+alias vi=vim
+
+echo "install fcitx rime"
+sudo add-apt-repository ppa:fcitx-team/stable && sudo apt-get update
+sudo apt-get install -y fcitx-rime
+echo "config fcitx..."
+im-config
+
+echo "install pip"
+sudo apt-get install -y python-pip
+
+echo "install git..."
+sudo apt-get install -y git
+
+echo "install ShadowSocks..."
+sudo apt-get install -y shadowsocks
+#pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+
+echo "start shadowsocks..."
+sudo sslocal -c /etc/shadowsocks.json -d start
+
+echo "add shadowsocks to startup..."
+#echo "sudo sslocal -c /etc/shadowsocks.json -d start" >> ~/.rc.local
+
+echo "install mp3,mp4..."
+sudo apt install ubuntu-restricted-extras
+
+#echo "add shadowsocks to startup..."
+#sudo echo "sudo sslocal -c /etc/shadowsocks.json -d start
+# >> /etc/init.d/rc.local 
+
+
+echo "install maven..."
+sudo apt install -y maven
+
+echo "install java, mkdir /usr/local/java directory..."
+sudo mkdir /usr/local/java/
+
+
+echo "config vim..."
+echo "set nu" >> ~/.vimrc
+echo "set autointend" >> ~/.vimrc
+
+echo "install upstart..."
+sudo apt install upstart
+
+
+
+echo "add shadowsocks to startup"
+sudo touch /etc/rc.local
+sudo chmod +x /etc/rc.local
+sudo echo "#!/bin/bash" >> /etc/rc.local
+sudo echo "sslocal -c /etc/shadowsocks.json -d start" >> /etc/rc.local
+sudo echo "exit 0"  >> /etc/rc.local
